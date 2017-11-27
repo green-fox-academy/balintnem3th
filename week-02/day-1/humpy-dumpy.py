85760a6c8b5e25a067ad820decd4a25d7c544f1f
balintnem3th <balint.nem3th@gmail.com>
2017-11-27 12:28:32 +0100

    1&2 feladata pythonban

---
+++ week-02/day-1/humpy-dumpy.py
@@ -0,0 +1,6 @@
+string0 ="All the king's horses and all the king's men" 
+string1="Humpty Dumpty had a great fall."
+string2="Humpty Dumpty sat on a wall,"
+string3="Couldn't put Humpty together again."
+
+print(string2 + '\n' +  string1 + '\n' + string0 + '\n' + string3 )
