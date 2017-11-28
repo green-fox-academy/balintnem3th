7f6eb99a21ef4d3c34f9ea1fbe9b635730977714
balintnem3th <balint.nem3th@gmail.com>
2017-11-28 09:21:21 +0100

    day-1 swap change + day-2 printer.py

--- week-02/day-1/swap.py
+++ week-02/day-1/swap.py
@@ -1,16 +1,8 @@
 a = 123
 b = 526
 
-c = a 
-print(a)
-print(b)
-# a &c = 123 b = 526
-b = a
 
-print(a)
-print(b)
-# a & b = 526   
-c = b 
-# b&c = 123
-print(a)
-print(b)
+a, b = b, a
+
+print(str(a))
+print(str(b))
---
+++ week-02/day-2/printer.py
@@ -0,0 +1,6 @@
+def printer(*args):
+    for a in args:
+        print(a)
+
+
+printer("slms","alal","alal",'a')
