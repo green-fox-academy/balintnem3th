b36beb2035e19d34e32d5c3c326b84f6241674b3
balintnem3th <balint.nem3th@gmail.com>
2017-11-28 17:38:04 +0100

    kesz a diagonalis is

--- week-02/day-2/diagonal_matrix.py
+++ week-02/day-2/diagonal_matrix.py
@@ -1,12 +1,16 @@
+helper=[]
+
 def diagonal_matrix(number):
-    matrix = [[0]* number]*number
-    for x in range(0,number-1):
-        for y in range(0,number-1):
-             if (x==y):
-                matrix[x][y]=1
-             else:
-                matrix[x][y]=0
-    print(matrix)
+    for x in range(0,number):
+        matrix = [0]* number
+        for y in range(0,number):
+
+            if y==x :
+                matrix[y]=1
+        
+        helper.append(matrix)
+    print(helper)
+    
 
 
 
