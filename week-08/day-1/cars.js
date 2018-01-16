e8961571c4b6f8e7c1f605512b1d1d2e86232822
Németh Bálint <balint.nem3th@gmail.com>
2018-01-16 17:31:56 +0100

    day-1 in folder

---
+++ week-08/day-1/cars.js
@@ -0,0 +1,75 @@
+'use strict';
+// 1st
+// Call the ride method of the volvo, with 42 as a parameter
+
+var volvo = {
+  type: "Volvo",
+  fuel: 23,
+  consumption: 0.06,
+  kms: 43000,
+  ride: function (km) {
+    this.kms += km;
+    this.fuel -= km * this.consumption; 
+  }
+};
+
+console.log(volvo.kms);
+volvo.ride(42);
+console.log(volvo.kms);
+
+
+
+// 2nd
+// Call the refuel function on the ferrari object using the bind method, with
+// 52 as a parameter
+
+var ferrari = {
+  type: "Ferrari",
+  fuel: 0,
+  consumption: 0.12,
+  kms: 9000,
+  ride: function (km) {
+    this.kms += km;
+    this.fuel -= km * this.consumption; 
+  }
+};
+
+function refuel(liters) {
+    this.fuel += liters
+  }
+
+console.log(ferrari.fuel);
+let newfunc = refuel.bind(ferrari);
+newfunc(52);
+
+console.log(ferrari.fuel);
+
+
+
+
+
+// 3rd
+// Create a tesla object that has 3 properties
+//  - type: string
+//  - battery: number
+//  - kms: number
+//  - consunption: number
+// And a method called ride, that takes a parameter celled km,
+// and increments kms with it, then drains the battery based on the consumpltion 
+var tesla = {
+    type: "roadster",
+    battery: 20,
+    kms: 100,
+    consumption: 50,
+    ride: function (km) {
+      this.kms += km;
+      this.battery -= km * this.consumption; 
+    }
+  };
+
+
+
+
+tesla.ride(36);
+console.log(tesla.kms);
+console.log(tesla.battery);
