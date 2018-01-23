c37363ae4736be86890b06f86d98b964d38ff1bb
Németh Bálint <balint.nem3th@gmail.com>
2018-01-23 19:52:24 +0100

    reddit-uj mappa

---
+++ week-08/day-4/Új mappa/add_post.js
@@ -0,0 +1,25 @@
+var apiUrl= "https://time-radish.glitch.me";
+
+var getData = function(){
+    var title = document.getElementById('input1').value;
+    var url = document.getElementById('input2').value;
+    var sendBack = {
+        'title': title,
+        'url': url
+    } 
+    console.log(sendBack);
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('POST', apiUrl+'/posts'); 
+    httpRequest.setRequestHeader('Accept','application/json');
+    httpRequest.setRequestHeader('Content-Type','application/json');
+    httpRequest.send(JSON.stringify(sendBack));
+    httpRequest.onreadystatechange = function(){
+        if(httpRequest.readyState === 4 && httpRequest.status === 200 ){
+            console.log('sikerult'); 
+        }
+    }
+}
+
+var button = document.querySelector('button');
+button.addEventListener('click',getData);
+
