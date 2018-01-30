46ae3aac2fc32f0018b0b6f18c5564b8999693fb
Németh Bálint <balint.nem3th@gmail.com>
2018-01-30 17:46:43 +0100

    fix palida exam

--- week-10/palida2/assets/index.js
+++ week-10/palida2/assets/index.js
@@ -4,6 +4,17 @@ var apiUrl= "http://localhost:8080";
  function getPlate(){
     var inputField = document.querySelector('.searchPlate');
     var plateVAriable= inputField.value;
+    var police = document.getElementById("policeCheck");
+    var diplomat = document.getElementById("diplomatCheck");
+    if(police.checked)
+    {
+        plateVAriable+='&police=1';
+    }
+    else if(diplomat.checked)
+    {
+        plateVAriable+='&diplomat=1';
+    }
+    console.log('!!!'+plateVAriable);
     var httpRequest = new XMLHttpRequest();
     httpRequest.open('GET', apiUrl+'/search?q='+plateVAriable );    
     httpRequest.send(null);
@@ -13,8 +24,9 @@ var apiUrl= "http://localhost:8080";
             console.log(data);
             var cont = document.querySelector('.container');
             removeChildrenFromNode(cont);
+            createHeader();
             data.forEach(function(i) {
-                createRow(i);
+            createRow(i);
             });
             
         };
@@ -23,6 +35,16 @@ var apiUrl= "http://localhost:8080";
 
 function getModel(param){
     console.log('getModel');
+    var police = document.getElementById("policeCheck");
+    var diplomat = document.getElementById("diplomatCheck");
+    if(police.checked)
+    {
+        param+='?police=1';
+    }
+    else if(diplomat.checked)
+    {
+        param+='?diplomat=1';
+    }
     var httpRequest = new XMLHttpRequest();
     httpRequest.open('GET', apiUrl+'/search/'+param);    
     httpRequest.send(null);
@@ -32,6 +54,7 @@ function getModel(param){
            // console.log('!!!');
             var cont = document.querySelector('.container');
             removeChildrenFromNode(cont);
+            createHeader();
             data.forEach(function(i) {
                 createRow(i);
             });  
@@ -57,6 +80,21 @@ cont.innerHTML += markup;
 
 };
 
+function createHeader(){
+   const header=`  
+    <table>
+            <tr>
+            <td>Plate:    </td>
+            <td>Brand:    </td>
+            <td>Model:    </td>
+            <td>Color:    </td>
+            <td>Year:     </td>
+            </tr>
+    </table>`;
+    var cont = document.querySelector('.container');
+    cont.innerHTML += header;
+}
+
 function removeChildrenFromNode(node) {
     if (node === undefined || node === null) {
         return;
@@ -66,4 +104,7 @@ function removeChildrenFromNode(node) {
     node.removeChild(node.firstChild);
     }
 }
- 
+
+function reload(){
+    location.reload();
+}
--- week-10/palida2/server.js
+++ week-10/palida2/server.js
@@ -13,71 +13,94 @@ var conn = mysql.createConnection({
     user: "'root'",
     password: "1234",
     database : "licence_plates"
-  });
+});
     
+conn.connect(function(err){
+if(err){
+    console.log("Error connecting to Db");
+    return;
+}
+console.log("Connection established");
+});
 
-  conn.connect(function(err){
-    if(err){
-      console.log("Error connecting to Db");
-      return;
-    }
-    console.log("Connection established");
-  });
+app.use('/assets', express.static('./assets'));
 
-//   app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
 app.get('/',function(req,res){
     console.log('runing');
+    res.status(200);
     res.sendFile(__dirname + '/index.html');
 });
 
-
-  conn.query("SELECT * FROM licence_plates;",function(err,rows){
+conn.query("SELECT * FROM licence_plates;",function(err,rows){
     console.log("Data received from Db:\n");
-
 });
   
+app.get('/search',function(req,res){
 
-  app.get('/search',function(req,res){
-    var plate = req.param('q');
-    var sql1 = "SELECT * FROM licence_plates WHERE plate LIKE '"+plate+"%'";
-   
-    if(plate === undefined){
-        res.json ({
-             "error": "Please provide an input!"
-         }) ;   
-     }
-     conn.query(sql1,function(err,rows){
-        if(err){
-        res.json ({
-            'error' : err
-            }) ;
-        }
-        res.json ({
-         'rows' : rows
+var plate = req.query.q;
+var police = req.query.police;  
+var diplomat = req.query.diplomat;
+
+var rb = 'RB';
+var dt = 'DT';
+
+
+if(police === undefined && diplomat === undefined){
+    var sql1 = "SELECT * FROM licence_plates WHERE plate LIKE '%"+plate+"%'";
+}
+else if(police !== undefined && diplomat === undefined){
+    var sql1 = "SELECT * FROM licence_plates WHERE plate LIKE '"+rb+"%' AND plate LIKE '%"+plate+"%'";
+}
+else {
+    var sql1 = "SELECT * FROM licence_plates WHERE plate LIKE '"+dt+"%' AND plate LIKE '%"+plate+"%'";
+} 
+    conn.query(sql1,function(err,rows){
+    if(err){
+    res.json ({
+        'error' : err
         }) ;
-      }); 
+    }
+    res.status(200);
+    res.json ({
+        'rows' : rows
+    }) ;
+    }); 
 });
-
 
 app.get('/search/:brand',function(req,res){
     var brand = req.params.brand;
-   // console.log(brand);
-    var sql2 = "SELECT * FROM licence_plates WHERE car_brand='"+brand+"'";
-     conn.query(sql2,function(err,rows){
-        if(err){
-        res.json ({
-            'error' : err
-            }) ;
-        }
-        res.status(200);
-        res.json ({
-         'rows' : rows
-        });
-      }); 
-  });
+    var police = req.query.police;  
+    var diplomat = req.query.diplomat;
+
+    const isDiplomat = parseInt(req.query.diplomat || 0) === 1;
+
+    console.log(police);
+    console.log(brand);
+    console.log(diplomat);
+    console.log(req.query);
+    console.log(isDiplomat);
+    var rb = 'RB';
+    var dt = 'DT';
+    if(police === undefined && diplomat === undefined){
+        var sql2 = "SELECT * FROM licence_plates WHERE car_brand='"+brand+"'";
+    }
+    else if(police !== undefined && diplomat === undefined){
+        var sql2 = "SELECT * FROM licence_plates WHERE plate LIKE '"+rb+"%' AND car_brand LIKE '%"+brand+"%'";
+    }
+    else {
+        var sql2 = "SELECT * FROM licence_plates WHERE plate LIKE '"+dt+"%' AND car_brand LIKE '%"+brand+"%'";
+    } 
+    conn.query(sql2,function(err,rows){
+    if(err){
+    res.json ({
+        'error' : err
+        }) ;
+    }
+    res.status(200);
+    res.json ({
+        'rows' : rows
+    });
+    }); 
+});
 
 app.listen(8080);
