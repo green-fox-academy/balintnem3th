b6ba60ad0f3cba6e5de35b8331ebd870b5b0002d
Németh Bálint <balint.nem3th@gmail.com>
2018-02-01 10:37:31 +0100

    import db to palida-reatake

---
+++ week-10/palida-re/clothing_js.sql
@@ -0,0 +1,43 @@
+CREATE TABLE warehouse(
+id INT NOT NULL AUTO_INCREMENT,
+item_name VARCHAR(80),
+manufacturer VARCHAR(30),
+category VARCHAR(30),
+size VARCHAR(4),
+unit_price FLOAT,
+in_store INT,
+PRIMARY KEY (id));
+GO
+
+INSERT INTO warehouse (item_name, manufacturer, category, size, unit_price, in_store)
+VALUES
+('Green Fox Jumper', 'Gucci', 'sweaters', 's', 80.00, 10),
+('Green Fox Jumper', 'Gucci', 'sweaters', 'm', 80.00, 10),
+('Half-Ip Ribbed Mock-Turtleneck Sweater', 'Polo Ralph Lauren', 'sweaters', 'l', 59.90, 10),
+('Green Fox Jumper', 'Gucci', 'sweaters', 'xxl', 80.00, 10),
+('Green Fox Short-Sleeved Mesh Polo Shirt', 'Ralph Lauren', 'shirts', 's', 60.50, 10),
+('Green Fox Short-Sleeved Mesh Polo Shirt', 'Ralph Lauren', 'shirts', 'm', 60.50, 10),
+('Wool/Alpaca Shawl Collar Cardigan', 'Bonnie Jean', 'sweaters', 'xl', 100.00, 10),
+('Green Fox Short-Sleeved Mesh Polo Shirt', 'Ralph Lauren', 'shirts', 'l', 60.50, 10),
+('Green Fox Short-Sleeved Mesh Polo Shirt', 'Ralph Lauren', 'shirts', 'xl', 60.50, 10),
+('Green Fox Jumper', 'Gucci', 'sweaters', 'l', 80.00, 10),
+('Green Fox Jumper', 'Gucci', 'sweaters', 'xl', 80.00, 10),
+('Green Fox Short-Sleeved Mesh Polo Shirt', 'Ralph Lauren', 'shirts', 's', 60.50, 10),
+('Two-Tone Sleeveless Sheath Dress', 'Nicole Miller', 'dresses', 'l', 69.90, 10),
+('Green Fox Short-Sleeved Mesh Polo Shirt', 'Ralph Lauren', 'shirts', 'm', 60.50, 10),
+('Green Fox Short-Sleeved Mesh Polo Shirt', 'Ralph Lauren', 'shirts', 'xl', 60.50, 10),
+('Strecth Steamed Pencil Skirt', 'Calvin Klein', 'skirts', 's', 39.00, 10),
+('Green Fox Short-Sleeved Mesh Polo Shirt', 'Ralph Lauren', 'shirts', 'l', 60.50, 10),
+('Strecth Steamed Pencil Skirt', 'Calvin Klein', 'skirts', 'm', 39.00, 10),
+('Half-Ip Ribbed Mock-Turtleneck Sweater', 'Polo Ralph Lauren', 'sweaters', 's', 59.90, 10),
+('Half-Ip Ribbed Mock-Turtleneck Sweater', 'Polo Ralph Lauren', 'sweaters', 'm', 59.90, 10),
+('Half-Ip Ribbed Mock-Turtleneck Sweater', 'Polo Ralph Lauren', 'sweaters', 'xxl', 59.90, 10),
+('Two-Tone Sleeveless Sheath Dress', 'Nicole Miller', 'dresses', 'xxl', 69.90, 10),
+('Two-Tone Sleeveless Sheath Dress', 'Nicole Miller', 'dresses', 'xl', 69.90, 10),
+('Two-Tone Sleeveless Sheath Dress', 'Nicole Miller', 'dresses', 's', 69.90, 10),
+('Wool/Alpaca Shawl Collar Cardigan', 'Bonnie Jean', 'sweaters', 's', 100.00, 10),
+('Half-Ip Ribbed Mock-Turtleneck Sweater', 'Polo Ralph Lauren', 'sweaters', 'xl', 59.90, 10),
+('Wool/Alpaca Shawl Collar Cardigan', 'Bonnie Jean', 'sweaters', 'm', 100.00, 10),
+('Wool/Alpaca Shawl Collar Cardigan', 'Bonnie Jean', 'sweaters', 'l', 100.00, 10),
+('Two-Tone Sleeveless Sheath Dress', 'Nicole Miller', 'dresses', 'm', 69.90, 10),
+('Wool/Alpaca Shawl Collar Cardigan', 'Bonnie Jean', 'sweaters', 'xxl', 100.00, 10);
