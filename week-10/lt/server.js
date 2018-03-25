d1b44d0e9bef781038e30d19073aedd7d79c7462
Németh Bálint <balint.nem3th@gmail.com>
2018-03-25 23:26:17 +0200

    add example for querys

--- week-10/lt/server.js
+++ week-10/lt/server.js
@@ -1,4 +1,4 @@
-'use strict';
+﻿'use strict';
 
 var express = require ('express');
 var bodyParser = require('body-parser');
@@ -20,36 +20,18 @@ app.get('/search/:firstId/:secondId',function(req,res){
 const firstId = req.param('firstId');
 const secondId = req.params.secondId;  
 const reqParams = req.params;
-console.log('First Id :',firstId);
-console.log('Second Id :',secondId);
-console.log('Request Params: ',reqParams);
 
 const queryParams = req.query; 
 const queryQ = req.param('product');
 const hasColor = req.query.hasOwnProperty('color');
 const blue = req.query.color;
 const hasUser = req.query.newuser === '';
- const notMemberOfQuery = req.query.anything;
-console.log('Teljes Query',queryParams);
-console.log('product',queryQ);
-console.log('Van Color ?',hasColor);
-console.log('Kék? ',blue);
-console.log('Üres a user? ',hasUser);
- console.log('Not member',notMemberOfQuery);
- const converted = ((notMemberOfQuery || 0) === 1);
-console.log('Nincs ilyen a queryben : ',notMemberOfQuery);
-console.log('Converted :',converted);
-console.log('is Converted ?',isConvertedReal);
+const notMemberOfQuery = req.query.anything;
 
-
-
-
-
-
-    res.status(200);
-    res.json({
-        'valami' : 'valami',
-     }); 
+  res.status(200);
+  res.json({
+      'valami' : 'valami',
+   }); 
 });
 
 app.listen(8080);
